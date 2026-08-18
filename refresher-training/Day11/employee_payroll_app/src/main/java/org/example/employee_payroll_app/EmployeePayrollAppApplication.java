91b7655097a50139acff5f3364c557a61c86a934
navam-18 <navam.srivastava_cs22@gla.ac.in>
2026-08-19 00:27:10 +0530

    [Navam Srivastava]Add. adding day 11 work

---
+++ refresher-training/Day11/employee_payroll_app/src/main/java/org/example/employee_payroll_app/EmployeePayrollAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.employee_payroll_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+	}
+
+}
