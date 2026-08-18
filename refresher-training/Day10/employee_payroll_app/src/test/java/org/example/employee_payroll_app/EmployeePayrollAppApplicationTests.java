afaa49403fd091776fa1afd445147126cfc3208b
navam-18 <navam.srivastava_cs22@gla.ac.in>
2026-08-19 00:24:03 +0530

    [Navam Srivastava]Add. adding day 10 work

---
+++ refresher-training/Day10/employee_payroll_app/src/test/java/org/example/employee_payroll_app/EmployeePayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.employee_payroll_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
